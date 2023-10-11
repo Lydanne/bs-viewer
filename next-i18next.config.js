41c4a4036a83da3249eda883f191f377bfc1ff7c
WumaCoder <15804854160@163.com>
2023-10-11 20:20:01 +0800

    chore: 安装i18n

---
+++ next-i18next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-i18next').UserConfig} */
+module.exports = {
+  i18n: {
+    defaultLocale: "zh",
+    locales: ["en", "zh"],
+  },
+  fallbackLng: {
+    default: ['zh'],
+  },
+};
--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
+const { i18n } = require("./next-i18next.config");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n,
   reactStrictMode: true,
-  transpilePackages: ['@douyinfe/semi-ui', '@douyinfe/semi-icons', '@douyinfe/semi-illustrations'],
-}
+  transpilePackages: [
+    "@douyinfe/semi-ui",
+    "@douyinfe/semi-icons",
+    "@douyinfe/semi-illustrations",
+  ],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
