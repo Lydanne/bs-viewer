f01ecc0f65c1f7f51493c521b5096fe5c3d629c5
WumaCoder <15804854160@163.com>
2023-10-14 15:19:58 +0800

    chore: 添加工具方法

--- utils/shared.ts
+++ utils/shared.ts
@@ -66,3 +66,22 @@ function dataURLToBlob(dataURL: string) {
   }
   return new Blob([arrayBuffer], { type: mimeString });
 }
+
+export function base64ToFile(base64: string, filename: string, mimeType: string) {
+  const arr = base64.split(",");
+  const mime = arr[0].match(/:(.*?);/)![1];
+  const bstr = atob(arr[1]);
+  let n = bstr.length;
+  const u8arr = new Uint8Array(n);
+  // eslint-disable-next-line no-plusplus
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
+  }
+  return new File([u8arr], filename, { type: mimeType });
+}
+export function downloadFile(file: File) {
+  const downloadLink = document.createElement("a");
+  downloadLink.href = URL.createObjectURL(file);
+  downloadLink.download = file.name;
+  downloadLink.click();
+}
