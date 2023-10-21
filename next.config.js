2627fc499c47a9dcd9548e76304fc5fc23e8bc7e
WumaCoder <15804854160@163.com>
2023-10-21 17:10:03 +0800

    refactor: 项目架构优化

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const { i18n } = require("./next-i18next.config");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n,
+  productionBrowserSourceMaps: true,
   reactStrictMode: true,
   transpilePackages: [
     "@douyinfe/semi-ui",
