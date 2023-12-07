13f5594e34ca41eef4a4cb7d5802b7a65d8e8f82
Carter <terrycarter1985@gmail.com>
2023-12-07 21:16:39 +0800

    modify next config

--- next.config.js
+++ next.config.js
@@ -4,20 +4,11 @@ const { i18n } = require("./next-i18next.config");
 const nextConfig = {
   // i18n,
   output: 'export',
-
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  trailingSlash: true,
-
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  skipTrailingSlashRedirect: true,
+  distDir: 'release',
 
   productionBrowserSourceMaps: true,
   reactStrictMode: true,
-  transpilePackages: [
-    "@douyinfe/semi-ui",
-    "@douyinfe/semi-icons",
-    "@douyinfe/semi-illustrations",
-  ],
-};
+  transpilePackages: ['@douyinfe/semi-ui', '@douyinfe/semi-icons', '@douyinfe/semi-illustrations'],
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
